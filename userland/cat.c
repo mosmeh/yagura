2acde52d01000a4edaefd2b5ce53b402a7b18c44
mosm <airman515@gmail.com>
2022-04-15 21:35:48 +0900

    cat: handle multiple arguments

--- userland/cat.c
+++ userland/cat.c
@@ -4,24 +4,16 @@
 
 #define BUF_SIZE 1024
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        dprintf(2, "Usage: cat filename\n");
-        return EXIT_FAILURE;
-    }
-
-    int fd = open(argv[1], O_RDONLY);
-    if (fd < 0) {
-        perror("open");
-        return EXIT_FAILURE;
-    }
+static int dump_file(const char* filename) {
+    int fd = open(filename, O_RDONLY);
+    if (fd < 0)
+        return -1;
     for (;;) {
         static char buf[BUF_SIZE];
         ssize_t nread = read(fd, buf, BUF_SIZE - 1);
         if (nread < 0) {
-            perror("read");
             close(fd);
-            return EXIT_FAILURE;
+            return -1;
         }
         if (nread == 0)
             break;
@@ -31,6 +23,19 @@ int main(int argc, char* argv[]) {
             break;
     }
     close(fd);
+    return 0;
+}
 
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        dprintf(2, "Usage: cat FILE...\n");
+        return EXIT_FAILURE;
+    }
+    for (int i = 1; i < argc; ++i) {
+        if (dump_file(argv[i])) {
+            perror("dump_file");
+            return EXIT_FAILURE;
+        }
+    }
     return EXIT_SUCCESS;
 }
