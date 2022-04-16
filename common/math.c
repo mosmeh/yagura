afb446234704e8662ca942e7500e29f3134f9216
mosm <airman515@gmail.com>
2022-04-16 19:22:41 +0900

    Implement sqrt and log2

---
+++ common/math.c
@@ -0,0 +1,17 @@
+#include <math.h>
+
+double sqrt(double x) {
+    double res;
+    __asm__("fsqrt" : "=t"(res) : "0"(x));
+    return res;
+}
+
+double log2(double x) {
+    double ret;
+    __asm__("fld1\n"
+            "fxch %%st(1)\n"
+            "fyl2x"
+            : "=t"(ret)
+            : "0"(x));
+    return ret;
+}
