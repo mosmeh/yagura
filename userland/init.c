5f8a119582c4f99ee3ce76b1415d45504b235451
mosm <airman515@gmail.com>
2022-04-16 19:30:03 +0900

    Show mouse cursor

--- userland/init.c
+++ userland/init.c
@@ -2,6 +2,19 @@
 #include "syscall.h"
 #include <kernel/api/fcntl.h>
 
+pid_t spawn(const char* filename) {
+    pid_t pid = fork();
+    if (pid == 0) {
+        static char* argv[] = {NULL};
+        static char* envp[] = {"PATH=/bin", "HOME=/root", NULL};
+        if (execve(filename, argv, envp) < 0) {
+            perror("execve");
+            abort();
+        }
+    }
+    return pid;
+}
+
 int main(void) {
     ASSERT(open("/dev/ttyS0", O_RDONLY) == 0);
     ASSERT(open("/dev/ttyS0", O_WRONLY) == 1);
@@ -9,16 +22,10 @@ int main(void) {
 
     chdir("/root");
 
+    spawn("/bin/mouse_cursor");
+
     for (;;) {
-        pid_t pid = fork();
-        if (pid == 0) {
-            static char* argv[] = {NULL};
-            static char* envp[] = {"PATH=/bin", "HOME=/root", NULL};
-            if (execve("/bin/sh", argv, envp) < 0) {
-                perror("execve");
-                abort();
-            }
-        }
+        pid_t pid = spawn("/bin/sh");
         if (waitpid(pid, NULL, 0) < 0) {
             perror("waitpid");
             return EXIT_FAILURE;
