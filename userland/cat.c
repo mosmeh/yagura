877ac0e0c944d596cbd6f87ca1a5045e0ca5a945
mosm <airman515@gmail.com>
2022-04-20 22:17:54 +0900

    cat: support reading from stdin

--- userland/cat.c
+++ userland/cat.c
@@ -1,18 +1,20 @@
 #include "stdlib.h"
 #include "syscall.h"
 #include <kernel/api/fcntl.h>
+#include <string.h>
 
 #define BUF_SIZE 1024
 
 static int dump_file(const char* filename) {
-    int fd = open(filename, O_RDONLY);
+    int fd = strcmp(filename, "-") ? open(filename, O_RDONLY) : 0;
     if (fd < 0)
         return -1;
     for (;;) {
         static char buf[BUF_SIZE];
         ssize_t nread = read(fd, buf, BUF_SIZE - 1);
         if (nread < 0) {
-            close(fd);
+            if (fd > 0)
+                close(fd);
             return -1;
         }
         if (nread == 0)
@@ -20,17 +22,21 @@ static int dump_file(const char* filename) {
         buf[nread] = '\0';
         printf("%s", buf);
     }
-    close(fd);
+    if (fd > 0)
+        close(fd);
     return 0;
 }
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        dprintf(2, "Usage: cat FILE...\n");
-        return EXIT_FAILURE;
+        if (dump_file("-") < 0) {
+            perror("dump_file");
+            return EXIT_FAILURE;
+        }
+        return EXIT_SUCCESS;
     }
     for (int i = 1; i < argc; ++i) {
-        if (dump_file(argv[i])) {
+        if (dump_file(argv[i]) < 0) {
             perror("dump_file");
             return EXIT_FAILURE;
         }
