d9a97cc14b45e6239aba03425b0d444e0f70e855
mosm <airman515@gmail.com>
2022-04-23 23:08:15 +0900

    Add "date" command

---
+++ userland/date.c
@@ -0,0 +1,15 @@
+#include "stdlib.h"
+#include <kernel/api/time.h>
+
+int main(void) {
+    time_t now;
+    if (time(&now) < 0) {
+        perror("time");
+        return EXIT_FAILURE;
+    }
+    struct tm tm;
+    char buf[1024];
+    asctime_r(gmtime_r(&now, &tm), buf);
+    puts(buf);
+    return EXIT_SUCCESS;
+}
