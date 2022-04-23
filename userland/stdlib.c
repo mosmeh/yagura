54453e578d1cb60e3c9a0fa7b5aa10341b45552c
mosm <airman515@gmail.com>
2022-04-23 23:07:28 +0900

    Implement "asctime_r" function

--- userland/stdlib.c
+++ userland/stdlib.c
@@ -296,3 +296,16 @@ struct tm* gmtime_r(const time_t* t, struct tm* tm) {
 
     return tm;
 }
+
+char* asctime_r(const struct tm* time_ptr, char* buf) {
+    static const char* day_names[] = {"Sun", "Mon", "Tue", "Wed",
+                                      "Thu", "Fri", "Sat"};
+    static const char* month_names[] = {"Jan", "Feb", "Mar", "Apr",
+                                        "May", "Jun", "Jul", "Aug",
+                                        "Sep", "Oct", "Nov", "Dec"};
+    int len = sprintf(
+        buf, "%s %s %2d %02d:%02d:%02d %d", day_names[time_ptr->tm_wday],
+        month_names[time_ptr->tm_mon], time_ptr->tm_mday, time_ptr->tm_hour,
+        time_ptr->tm_min, time_ptr->tm_sec, time_ptr->tm_year + 1900);
+    return len > 0 ? buf : NULL;
+}
--- userland/stdlib.h
+++ userland/stdlib.h
@@ -50,3 +50,4 @@ clock_t clock(void);
 
 time_t time(time_t* tloc);
 struct tm* gmtime_r(time_t const* t, struct tm* tm);
+char* asctime_r(const struct tm* time_ptr, char* buf);
