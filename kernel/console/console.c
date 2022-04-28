665125a398576af759407f04e7d94d9d2b9791a6
mosm <airman515@gmail.com>
2022-04-28 22:34:15 +0900

    console: connect to either tty or ttyS0
    /dev/ttyS0 can be quite slow, which makes /dev/console slow even when
    we are interacting only with /dev/tty.
    So let's just stop writing to both consoles.

--- kernel/console/console.c
+++ kernel/console/console.c
@@ -5,59 +5,30 @@
 #include <kernel/fs/fs.h>
 #include <kernel/memory/memory.h>
 
-static file_description* tty = NULL;
-static file_description* ttyS0 = NULL;
+static file_description* active_console = NULL;
 
 void console_init(void) {
-    file_description* desc = vfs_open("/dev/ttyS0", O_RDWR, 0);
-    if (IS_OK(desc))
-        ttyS0 = desc;
-    else
-        ASSERT(PTR_ERR(desc) == -ENOENT);
+    active_console = vfs_open("/dev/tty", O_RDWR, 0);
+    if (IS_OK(active_console))
+        return;
 
-    desc = vfs_open("/dev/tty", O_RDWR, 0);
-    if (IS_OK(desc))
-        tty = desc;
-    else
-        ASSERT(PTR_ERR(desc) == -ENOENT);
-}
+    active_console = vfs_open("/dev/ttyS0", O_RDWR, 0);
+    if (IS_OK(active_console))
+        return;
 
-static ssize_t write_all(file_description* desc, const char* s, size_t count) {
-    size_t total_nwritten = 0;
-    while (total_nwritten < count) {
-        ssize_t nwritten = fs_write(desc, s, count);
-        if (IS_ERR(nwritten))
-            return nwritten;
-        s += nwritten;
-        total_nwritten += nwritten;
-    }
-    return count;
+    UNIMPLEMENTED();
 }
 
 static ssize_t console_device_read(file_description* desc, void* buffer,
                                    size_t count) {
     (void)desc;
-    if (tty)
-        return fs_read(tty, buffer, count);
-    if (ttyS0)
-        return fs_read(ttyS0, buffer, count);
-    return 0;
+    return fs_read(active_console, buffer, count);
 }
 
 static ssize_t console_device_write(file_description* desc, const void* buffer,
                                     size_t count) {
     (void)desc;
-    if (ttyS0) {
-        ssize_t nwritten = write_all(ttyS0, buffer, count);
-        if (IS_ERR(nwritten))
-            return nwritten;
-    }
-    if (tty) {
-        ssize_t nwritten = write_all(tty, buffer, count);
-        if (IS_ERR(nwritten))
-            return nwritten;
-    }
-    return count;
+    return fs_write(active_console, buffer, count);
 }
 
 struct file* console_device_create(void) {
