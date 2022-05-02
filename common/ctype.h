c1497d611cd0484f8990921671f7dbca9e36599d
mosm <airman515@gmail.com>
2022-05-02 23:10:10 +0900

    Implement functions "tolower" and "toupper"

--- common/ctype.h
+++ common/ctype.h
@@ -13,4 +13,16 @@ static inline int isspace(int c) {
     return 0;
 }
 
-static inline int isprint(int c) { return 0x20 <= c && c <= 0x7e; }
+static inline int isprint(int c) { return (int)(0x20 <= c && c <= 0x7e); }
+
+static inline int tolower(int c) {
+    if ('A' <= c && c <= 'Z')
+        return c | 0x20;
+    return c;
+}
+
+static inline int toupper(int c) {
+    if ('a' <= c && c <= 'z')
+        return c & ~0x20;
+    return c;
+}
