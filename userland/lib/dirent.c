7981c498d27f2bdb98e178ffe021cd7e0ffa045f
mosm <airman515@gmail.com>
2022-05-08 01:02:43 +0900

    readdir: handle EINVAL from getdents

--- userland/lib/dirent.c
+++ userland/lib/dirent.c
@@ -1,14 +1,13 @@
 #include "dirent.h"
+#include "errno.h"
 #include "stdlib.h"
 #include "sys/fcntl.h"
 #include "unistd.h"
 
-#define DIR_BUF_CAPACITY 1024
-
 typedef struct DIR {
     int fd;
-    unsigned char buf[DIR_BUF_CAPACITY];
-    size_t buf_size;
+    unsigned char* buf;
+    size_t buf_capacity, buf_size;
     size_t buf_cursor;
 } DIR;
 
@@ -16,24 +15,44 @@ DIR* opendir(const char* name) {
     DIR* dirp = malloc(sizeof(DIR));
     if (!dirp)
         return NULL;
+    *dirp = (DIR){0};
     dirp->fd = open(name, O_RDONLY);
     if (dirp->fd < 0)
         return NULL;
+    dirp->buf_capacity = 1024;
     return dirp;
 }
 
 int closedir(DIR* dirp) {
     int rc = close(dirp->fd);
+    free(dirp->buf);
     free(dirp);
     return rc;
 }
 
 struct dirent* readdir(DIR* dirp) {
     if (dirp->buf_cursor >= dirp->buf_size) {
-        ssize_t nwritten = getdents(dirp->fd, dirp->buf, DIR_BUF_CAPACITY);
-        if (nwritten <= 0)
-            return NULL;
-        dirp->buf_size = nwritten;
+        int saved_errno = errno;
+        for (;;) {
+            unsigned char* new_buf = realloc(dirp->buf, dirp->buf_capacity);
+            if (!new_buf)
+                return NULL;
+            dirp->buf = new_buf;
+            errno = 0;
+            ssize_t nread = getdents(dirp->fd, dirp->buf, dirp->buf_capacity);
+            if (nread == 0) {
+                errno = saved_errno;
+                return NULL;
+            }
+            if (nread > 0) {
+                dirp->buf_size = nread;
+                break;
+            }
+            if (errno != EINVAL)
+                return NULL;
+            dirp->buf_capacity *= 2;
+        }
+        errno = saved_errno;
         dirp->buf_cursor = 0;
     }
     struct dirent* dent = (struct dirent*)(dirp->buf + dirp->buf_cursor);
