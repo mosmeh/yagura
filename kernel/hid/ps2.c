cbe61c9dfd9b92da1fd86d6da4296f20625f3fc1
mosm <airman515@gmail.com>
2022-05-20 22:54:44 +0900

    ps2: drain output buffer before initializations
    Garbage data in the buffer sometimes caused the initialization to fail.

--- kernel/hid/ps2.c
+++ kernel/hid/ps2.c
@@ -3,16 +3,19 @@
 void ps2_keyboard_init(void);
 void ps2_mouse_init(void);
 
-void ps2_init(void) {
-    ps2_write(PS2_COMMAND, PS2_DISABLE_PORT1);
-    ps2_write(PS2_COMMAND, PS2_DISABLE_PORT2);
-
-    // drain the buffer
+static void drain_output_buffer(void) {
     for (int timeout = 0; timeout < 1024; ++timeout) {
         if (!(in8(PS2_STATUS) & 1))
             break;
         in8(PS2_DATA);
     }
+}
+
+void ps2_init(void) {
+    ps2_write(PS2_COMMAND, PS2_DISABLE_PORT1);
+    ps2_write(PS2_COMMAND, PS2_DISABLE_PORT2);
+
+    drain_output_buffer();
 
     ps2_write(PS2_COMMAND, PS2_READ_CONFIG);
     uint8_t config = ps2_read(PS2_DATA);
@@ -20,6 +23,9 @@ void ps2_init(void) {
     ps2_write(PS2_COMMAND, PS2_WRITE_CONFIG);
     ps2_write(PS2_DATA, config);
 
+    drain_output_buffer();
     ps2_keyboard_init();
+
+    drain_output_buffer();
     ps2_mouse_init();
 }
