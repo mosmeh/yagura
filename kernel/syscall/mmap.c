10860f4a7bd2e2e0b1af33519a164a6bdb9a66ae
mosm <airman515@gmail.com>
2023-04-23 10:43:50 +0900

    syscall(mmap): check for EFAULT

--- kernel/syscall/mmap.c
+++ kernel/syscall/mmap.c
@@ -6,51 +6,56 @@
 #include <kernel/boot_defs.h>
 #include <kernel/memory/memory.h>
 #include <kernel/process.h>
+#include <kernel/safe_string.h>
 #include <string.h>
 
-void* sys_mmap(const mmap_params* params) {
-    if (params->length == 0 || params->offset < 0 ||
-        (params->offset % PAGE_SIZE) ||
-        !((params->flags & MAP_PRIVATE) ^ (params->flags & MAP_SHARED)))
+void* sys_mmap(const mmap_params* user_params) {
+    mmap_params params;
+    if (!copy_from_user(&params, user_params, sizeof(mmap_params)))
         return ERR_PTR(-EINVAL);
 
-    if ((params->flags & MAP_FIXED) || !(params->prot & PROT_READ))
+    if (params.length == 0 || params.offset < 0 ||
+        (params.offset % PAGE_SIZE) ||
+        !((params.flags & MAP_PRIVATE) ^ (params.flags & MAP_SHARED)))
+        return ERR_PTR(-EINVAL);
+
+    if ((params.flags & MAP_FIXED) || !(params.prot & PROT_READ))
         return ERR_PTR(-ENOTSUP);
-    if ((params->flags & MAP_ANONYMOUS) && (params->offset != 0))
+    if ((params.flags & MAP_ANONYMOUS) && (params.offset != 0))
         return ERR_PTR(-ENOTSUP);
 
     uint16_t page_flags = PAGE_USER;
-    if (params->prot & PROT_WRITE)
+    if (params.prot & PROT_WRITE)
         page_flags |= PAGE_WRITE;
-    if (params->flags & MAP_SHARED)
+    if (params.flags & MAP_SHARED)
         page_flags |= PAGE_SHARED;
 
-    if (params->flags & MAP_ANONYMOUS) {
+    if (params.flags & MAP_ANONYMOUS) {
         uintptr_t addr =
-            range_allocator_alloc(&current->vaddr_allocator, params->length);
+            range_allocator_alloc(&current->vaddr_allocator, params.length);
         if (IS_ERR(addr))
             return ERR_PTR(addr);
 
-        int rc = paging_map_to_free_pages(addr, params->length, page_flags);
+        int rc = paging_map_to_free_pages(addr, params.length, page_flags);
         if (IS_ERR(rc))
             return ERR_PTR(rc);
 
-        memset((void*)addr, 0, params->length);
+        memset((void*)addr, 0, params.length);
         return (void*)addr;
     }
 
-    file_description* desc = process_get_file_description(params->fd);
+    file_description* desc = process_get_file_description(params.fd);
     if (IS_ERR(desc))
         return desc;
     if (S_ISDIR(desc->inode->mode))
         return ERR_PTR(-ENODEV);
 
     uintptr_t addr =
-        range_allocator_alloc(&current->vaddr_allocator, params->length);
+        range_allocator_alloc(&current->vaddr_allocator, params.length);
     if (IS_ERR(addr))
         return ERR_PTR(addr);
 
-    int rc = file_description_mmap(desc, addr, params->length, params->offset,
+    int rc = file_description_mmap(desc, addr, params.length, params.offset,
                                    page_flags);
     if (IS_ERR(rc))
         return ERR_PTR(rc);
