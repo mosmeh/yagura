079dca198b55444099ebdb4ed2d5b7937d89943d
Yuta Imazu <yuta.imazu@gmail.com>
2025-12-20 21:20:13 +0900

    [skip ci] prepare

--- userland/init.c
+++ userland/init.c
@@ -109,6 +109,18 @@ int main(void) {
     for (size_t i = 0; i < ARRAY_SIZE(device_files); ++i)
         try_mknod(&device_files[i]);
 
+    {
+        pid_t pid = do_spawn("/bin/busybox", (char*[]){
+                                                 "/bin/busybox",
+                                                 "--install",
+                                                 "-s",
+                                                 "/bin",
+                                                 NULL,
+                                             });
+        ASSERT_OK(pid);
+        waitpid(pid, NULL, 0);
+    }
+
     for (size_t i = 0; i < 64; ++i) {
         char pathname[16];
         (void)sprintf(pathname, "/dev/tty%u", i);
