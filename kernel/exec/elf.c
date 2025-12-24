05f9d91ed38cac465a7d0167c89e5aebc16c0f85
Yuta Imazu <yuta.imazu@gmail.com>
2025-12-25 04:55:30 +0900

    exec(elf): support PT_INTERP and PIE

--- kernel/exec/elf.c
+++ kernel/exec/elf.c
@@ -7,6 +7,10 @@
 #include <kernel/system.h>
 #include <kernel/time.h>
 
+#define ELF_ET_DYN_BASE 0x400000
+
+#define INVALID_ADDR ((void*)(-1))
+
 NODISCARD static int validate_ehdr(const Elf32_Ehdr* ehdr) {
     if (!IS_ELF(*ehdr) || ehdr->e_ident[EI_CLASS] != ELFCLASS32 ||
         ehdr->e_ident[EI_DATA] != ELFDATA2LSB ||
@@ -35,29 +39,72 @@ NODISCARD static int validate_ehdr(const Elf32_Ehdr* ehdr) {
     return 0;
 }
 
+NODISCARD static const char* find_interp(const struct exec_image* image) {
+    const Elf32_Ehdr* ehdr = (const void*)image->data;
+    const Elf32_Phdr* phdr = (const void*)(image->data + ehdr->e_phoff);
+    for (size_t i = 0; i < ehdr->e_phnum; ++i, ++phdr) {
+        if (phdr->p_type != PT_INTERP)
+            continue;
+        if (phdr->p_filesz == 0 || phdr->p_filesz > PATH_MAX)
+            return ERR_PTR(-ENOEXEC);
+        const char* interp = (const void*)(image->data + phdr->p_offset);
+        if (interp[phdr->p_filesz - 1] != 0)
+            return ERR_PTR(-ENOEXEC);
+        return interp;
+    }
+    return NULL;
+}
+
 NODISCARD
-static int load_segments(struct loader* loader, void** out_phdr_addr) {
-    const Elf32_Ehdr* ehdr = (const void*)loader->image.data;
-    const Elf32_Phdr* phdr = (const void*)(loader->image.data + ehdr->e_phoff);
-    void* phdr_addr = NULL;
+static int load_segments(struct vm* vm, const struct exec_image* image,
+                         bool fixed, size_t* inout_base, void** out_phdr_addr) {
+    const Elf32_Ehdr* ehdr = (const void*)image->data;
+    const Elf32_Phdr* phdr = (const void*)(image->data + ehdr->e_phoff);
+
+    unsigned char* base_addr = INVALID_ADDR;
+    unsigned char* phdr_addr = INVALID_ADDR;
+
     for (size_t i = 0; i < ehdr->e_phnum; ++i, ++phdr) {
         if (phdr->p_type != PT_LOAD)
             continue;
         if (phdr->p_filesz > phdr->p_memsz)
             return -ENOEXEC;
 
-        if (phdr->p_offset <= ehdr->e_phoff &&
-            ehdr->e_phoff < phdr->p_offset + phdr->p_filesz)
-            phdr_addr = (void*)(ehdr->e_phoff - phdr->p_offset + phdr->p_vaddr);
+        // For the first segment:
+        //  - If fixed is true, use the specified virtual address
+        //  - If fixed is false, let the VM choose the address and set base_addr
+        // For subsequent segments:
+        //  - Calculate the virtual address relative to base_addr
+
+        uintptr_t vaddr = phdr->p_vaddr;
+        if (base_addr == INVALID_ADDR)
+            vaddr += inout_base ? *inout_base : 0;
+        else
+            vaddr += (uintptr_t)base_addr;
 
         if (phdr->p_filesz > 0) {
-            uintptr_t start = ROUND_DOWN(phdr->p_vaddr, PAGE_SIZE);
-            size_t npages =
-                DIV_CEIL(phdr->p_filesz + (phdr->p_vaddr - start), PAGE_SIZE);
-            struct vm_region* region =
-                vm_alloc_at(loader->vm, (void*)start, npages);
-            if (IS_ERR(ASSERT(region)))
-                return PTR_ERR(region);
+            size_t page_offset = vaddr % PAGE_SIZE;
+            size_t npages = DIV_CEIL(phdr->p_filesz + page_offset, PAGE_SIZE);
+
+            struct vm_region* region;
+            uintptr_t region_start;
+            if (fixed || base_addr != INVALID_ADDR) {
+                region_start = ROUND_DOWN(vaddr, PAGE_SIZE);
+                region = vm_alloc_at(vm, (void*)region_start, npages);
+                if (IS_ERR(ASSERT(region)))
+                    return PTR_ERR(region);
+            } else {
+                // Let the VM choose the address
+                region = vm_alloc(vm, npages);
+                if (IS_ERR(ASSERT(region)))
+                    return PTR_ERR(region);
+                region_start = (uintptr_t)vm_region_to_virt(region);
+                vaddr = region_start + page_offset;
+            }
+            if (base_addr == INVALID_ADDR) {
+                base_addr = (void*)(region_start -
+                                    ROUND_DOWN(phdr->p_vaddr, PAGE_SIZE));
+            }
 
             unsigned vm_flags = VM_USER;
             if (phdr->p_flags & PF_R)
@@ -68,11 +115,10 @@ static int load_segments(struct loader* loader, void** out_phdr_addr) {
             if (IS_ERR(rc))
                 return rc;
 
-            size_t offset_bytes = phdr->p_offset - (phdr->p_vaddr - start);
-            vm_region_set_obj(region, loader->image.obj,
-                              offset_bytes >> PAGE_SHIFT);
+            size_t offset_bytes = phdr->p_offset - page_offset;
+            vm_region_set_obj(region, image->obj, offset_bytes >> PAGE_SHIFT);
 
-            uintptr_t zero_start = phdr->p_vaddr + phdr->p_filesz;
+            uintptr_t zero_start = vaddr + phdr->p_filesz;
             uintptr_t zero_end = ROUND_UP(zero_start, PAGE_SIZE);
             if (zero_start < zero_end && (vm_flags & VM_WRITE)) {
                 // Zero the remaining bytes in the last page
@@ -81,14 +127,22 @@ static int load_segments(struct loader* loader, void** out_phdr_addr) {
             }
         }
 
+        if (phdr->p_offset <= ehdr->e_phoff &&
+            ehdr->e_phoff < phdr->p_offset + phdr->p_filesz)
+            phdr_addr = (void*)(ehdr->e_phoff - phdr->p_offset + vaddr);
+
         // Map anonymous pages for the BSS section
-        uintptr_t zero_start =
-            ROUND_UP(phdr->p_vaddr + phdr->p_filesz, PAGE_SIZE);
-        uintptr_t zero_end = ROUND_UP(phdr->p_vaddr + phdr->p_memsz, PAGE_SIZE);
+        uintptr_t zero_start = ROUND_UP(vaddr + phdr->p_filesz, PAGE_SIZE);
+        uintptr_t zero_end = ROUND_UP(vaddr + phdr->p_memsz, PAGE_SIZE);
         if (zero_start < zero_end) {
+            if (base_addr == INVALID_ADDR) {
+                // We encountered BSS before any file-backed PT_LOAD segment
+                return -ENOEXEC;
+            }
+
             size_t npages = (zero_end - zero_start) >> PAGE_SHIFT;
             struct vm_region* region =
-                vm_alloc_at(loader->vm, (void*)zero_start, npages);
+                vm_alloc_at(vm, (void*)zero_start, npages);
             if (IS_ERR(ASSERT(region)))
                 return PTR_ERR(region);
 
@@ -104,14 +158,37 @@ static int load_segments(struct loader* loader, void** out_phdr_addr) {
             vm_region_set_obj(region, anon, 0);
         }
     }
+
+    if (base_addr == INVALID_ADDR || phdr_addr == INVALID_ADDR)
+        return -ENOEXEC;
+
+    if (inout_base)
+        *inout_base = (size_t)base_addr;
     if (out_phdr_addr)
         *out_phdr_addr = phdr_addr;
     return 0;
 }
 
+NODISCARD static ssize_t load_interp(struct loader* loader,
+                                     const struct exec_image* image) {
+    const Elf32_Ehdr* ehdr = (const void*)image->data;
+    int rc = validate_ehdr(ehdr);
+    if (IS_ERR(rc))
+        return rc;
+
+    size_t base = 0;
+    rc = load_segments(loader->vm, image, false, &base, NULL);
+    if (IS_ERR(rc))
+        return rc;
+
+    loader->entry_point = (void*)(ehdr->e_entry + base);
+    return base;
+}
+
 NODISCARD
 static int populate_stack(struct loader* loader, const Elf32_Ehdr* ehdr,
-                          void* phdr_addr) {
+                          void* phdr_addr, size_t interp_base,
+                          void* entry_point) {
     loader->stack_ptr = (void*)ROUND_DOWN((uintptr_t)loader->stack_ptr, 16);
 
     unsigned char* random;
@@ -133,7 +210,8 @@ static int populate_stack(struct loader* loader, const Elf32_Ehdr* ehdr,
         {AT_PHENT, {ehdr->e_phentsize}},
         {AT_PHNUM, {ehdr->e_phnum}},
         {AT_PAGESZ, {PAGE_SIZE}},
-        {AT_ENTRY, {ehdr->e_entry}},
+        {AT_BASE, {interp_base}},
+        {AT_ENTRY, {(uint32_t)entry_point}},
         {AT_UID, {0}},
         {AT_EUID, {0}},
         {AT_GID, {0}},
@@ -202,14 +280,48 @@ int elf_load(struct loader* loader) {
     if (IS_ERR(rc))
         return rc;
 
+    const char* interp = find_interp(&loader->image);
+    if (IS_ERR(interp))
+        return PTR_ERR(interp);
+
     void* phdr_addr = NULL;
-    rc = load_segments(loader, &phdr_addr);
+    size_t exec_base = 0;
+    bool exec_fixed = false;
+    switch (ehdr->e_type) {
+    case ET_EXEC:
+        exec_fixed = true;
+        break;
+    case ET_DYN:
+        if (interp) {
+            exec_base = ELF_ET_DYN_BASE;
+            exec_fixed = true;
+        }
+        break;
+    default:
+        return -ENOEXEC;
+    }
+    rc = load_segments(loader->vm, &loader->image, exec_fixed, &exec_base,
+                       &phdr_addr);
     if (IS_ERR(rc))
         return rc;
 
-    loader->entry_point = (void*)ehdr->e_entry;
+    void* entry_point = (void*)(ehdr->e_entry + exec_base);
+    loader->entry_point = entry_point;
 
-    rc = populate_stack(loader, ehdr, phdr_addr);
+    size_t interp_base = 0;
+    if (interp) {
+        struct exec_image interp_image = {0};
+        rc = exec_image_load(&interp_image, interp);
+        if (IS_ERR(rc))
+            return rc;
+        ssize_t base = load_interp(loader, &interp_image);
+        exec_image_unload(&interp_image);
+        if (IS_ERR(base))
+            return base;
+        interp_base = base;
+    }
+
+    rc = populate_stack(loader, ehdr, phdr_addr, interp_base, entry_point);
     if (IS_ERR(rc))
         return rc;
 
