a28fb98badd8cba6d20678df4f58f702c9e06f1d
Yuta Imazu <yuta.imazu@gmail.com>
2026-01-03 11:04:32 +0900

    meta: avoid QEMU crash when running with WHPX

--- run.sh
+++ run.sh
@@ -6,39 +6,42 @@ KERNEL='kernel/kernel'
 INITRD='initrd'
 CMDLINE=()
 NUM_CPUS=1
+QEMU_EXTRA_ARGS=()
 
 case "$1" in
     shell) # Serial console
-        QEMU_DISPLAY_ARGS=(-display none -vga none)
+        QEMU_EXTRA_ARGS+=(-display none -vga none)
         CMDLINE+=(console=ttyS0)
         ;;
     text) # Text console
-        QEMU_DISPLAY_ARGS=(-display "sdl,gl=off" -vga cirrus)
+        QEMU_EXTRA_ARGS+=(-display "sdl,gl=off" -vga cirrus)
         CMDLINE+=(console=tty1)
         ;;
     *) # Framebuffer console
-        QEMU_DISPLAY_ARGS=(-display "sdl,gl=off,show-cursor=off")
+        QEMU_EXTRA_ARGS+=(-display "sdl,gl=off,show-cursor=off")
         CMDLINE+=(console=tty1)
         ;;
 esac
 
 QEMU_BINARY_PREFIX=''
 QEMU_BINARY_SUFFIX=''
-QEMU_VIRT_TECH_ARGS=()
-
-if [ -r /dev/kvm ] && [ -w /dev/kvm ] &&
-    command -v kvm-ok >/dev/null && kvm-ok &>/dev/null; then
-    QEMU_VIRT_TECH_ARGS=(-enable-kvm)
-fi
 
 if command -v wslpath >/dev/null; then
     PATH=${PATH}:/mnt/c/Windows/System32
     QEMU_INSTALL_DIR=$(reg.exe query 'HKLM\Software\QEMU' /v Install_Dir /t REG_SZ | grep '^    Install_Dir' | sed 's/    / /g' | cut -f4- -d' ')
     QEMU_BINARY_PREFIX="$(wslpath -- "${QEMU_INSTALL_DIR}" | tr -d '\r\n')/"
     QEMU_BINARY_SUFFIX='.exe'
-    QEMU_VIRT_TECH_ARGS=(-accel "whpx,kernel-irqchip=off" -accel tcg)
+    QEMU_EXTRA_ARGS+=(-accel "whpx,kernel-irqchip=off" -accel tcg)
     KERNEL=$(wslpath -w "${KERNEL}")
     INITRD=$(wslpath -w "${INITRD}")
+else
+    # NOTE: -cpu max results in "Unexpected VP exit code 4" error when used with WHPX
+    QEMU_EXTRA_ARGS+=(-cpu max)
+
+    if [ -r /dev/kvm ] && [ -w /dev/kvm ] &&
+        command -v kvm-ok >/dev/null && kvm-ok &>/dev/null; then
+        QEMU_EXTRA_ARGS+=(-enable-kvm)
+    fi
 fi
 
 QEMU_BIN="${QEMU_BINARY_PREFIX}qemu-system-i386${QEMU_BINARY_SUFFIX}"
@@ -47,12 +50,10 @@ QEMU_BIN="${QEMU_BINARY_PREFIX}qemu-system-i386${QEMU_BINARY_SUFFIX}"
     -initrd "${INITRD}" \
     -append "${CMDLINE[*]}" \
     -d guest_errors \
-    "${QEMU_DISPLAY_ARGS[@]}" \
     -device ac97 \
     -chardev stdio,mux=on,id=char0 \
     -serial chardev:char0 \
     -mon char0,mode=readline \
-    -cpu max \
     -m 512M \
     -smp "sockets=1,cores=${NUM_CPUS},threads=1" \
-    "${QEMU_VIRT_TECH_ARGS[@]}"
+    "${QEMU_EXTRA_ARGS[@]}"
