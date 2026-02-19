21875ff9f8702fbe877aca0f59546912a7a9879c
Yuta Imazu <yuta.imazu@gmail.com>
2026-02-19 21:18:16 +0900

    x86: use rate generator mode of PIT
    On QEMU with TCG, PIT square wave generator mode (mode 3) combined with
    I/O APIC seems to tick twice as fast as it should. This is not the case
    on my real x86_64 machine or with KVM.
    This does not seem to happen with rate generator mode (mode 2).

--- kernel/arch/x86/interrupts/pit.c
+++ kernel/arch/x86/interrupts/pit.c
@@ -7,8 +7,10 @@
 #define PIT_CTL 0x43
 #define TIMER0_SELECT 0x00
 #define WRITE_WORD 0x30
-#define MODE_SQUARE_WAVE 0x06
+#define MODE_RATE_GENERATOR 0x04
 #define BASE_FREQUENCY 1193182
+
+STATIC_ASSERT(CLK_TCK <= BASE_FREQUENCY);
 
 static void tick(struct registers* regs) {
     time_tick();
@@ -17,7 +19,7 @@ static void tick(struct registers* regs) {
 
 void pit_init(void) {
     uint16_t div = BASE_FREQUENCY / CLK_TCK;
-    out8(PIT_CTL, TIMER0_SELECT | WRITE_WORD | MODE_SQUARE_WAVE);
+    out8(PIT_CTL, TIMER0_SELECT | WRITE_WORD | MODE_RATE_GENERATOR);
     out8(TIMER0_CTL, div & 0xff);
     out8(TIMER0_CTL, div >> 8);
     arch_interrupts_set_handler(IRQ(0), tick);
