05928fae851f93695b9fca5b739536b2d242563e
Yuta Imazu <yuta.imazu@gmail.com>
2026-02-24 02:37:26 +0900

    common(integer): make ROUND_{UP,DOWN} support non-power-of-two alignment

--- common/integer.h
+++ common/integer.h
@@ -5,8 +5,19 @@
 #include <common/stddef.h>
 #include <common/stdint.h>
 
-#define ROUND_UP(x, align) (((x) + ((align) - 1)) & ~((align) - 1))
-#define ROUND_DOWN(x, align) ((x) & ~((align) - 1))
+#define ROUND_UP(x, align)                                                     \
+    __extension__({                                                            \
+        __typeof__(x) __x = (x);                                               \
+        __typeof__(align) __align = (align);                                   \
+        __typeof__(__x % __align) __mod = __x % __align;                       \
+        __mod == 0 ? __x : __x + (__align - __mod);                            \
+    })
+#define ROUND_DOWN(x, align)                                                   \
+    __extension__({                                                            \
+        __typeof__(x) __x = (x);                                               \
+        __x - (__x % (align));                                                 \
+    })
+
 #define DIV_CEIL(lhs, rhs) (((lhs) + (rhs) - 1) / (rhs))
 
 static inline size_t next_power_of_two(size_t x) {
