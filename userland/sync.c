210008d15733f257cf26ea13f849a7af9d91ce61
Yuta Imazu <yuta.imazu@gmail.com>
2026-03-01 05:16:36 +0900

    sync: support fsync

--- userland/sync.c
+++ userland/sync.c
@@ -1,6 +1,26 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-int main(void) {
-    sync();
-    return 0;
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        sync();
+        return EXIT_SUCCESS;
+    }
+    for (int i = 1; i < argc; ++i) {
+        int fd = open(argv[i], O_RDONLY);
+        if (fd < 0) {
+            perror("open");
+            return EXIT_FAILURE;
+        }
+        int result = fsync(fd);
+        if (result < 0) {
+            perror("fsync");
+            close(fd);
+            return EXIT_FAILURE;
+        }
+        close(fd);
+    }
+    return EXIT_SUCCESS;
 }
